bb819a221ed1e0b18197e4ba770e0b7b015239eb
jgrynczewski <jgrynczewski@gmail.com>
2024-12-09 14:34:12 +0100

    static files

--- hello_app/urls.py
+++ hello_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path("", views.hello),
     path("param/<str:name>/", views.hello_name),
     path("template/<str:name>/", views.hello_template),
+    path(
+        "isitmonday/",
+        views.is_it_monday
+    ),
 ]
--- hello_app/views.py
+++ hello_app/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.http import HttpResponse
 from django.utils.html import escape
 from django.shortcuts import render
@@ -16,3 +18,15 @@ def hello_name(request, name):
 def hello_template(request, name):
     # return HttpResponse("<html><head></head><body><h1>Hello, world!</h1></body></html>")
     return render(request, "hello.html", context={"name": name})
+
+
+def is_it_monday(request):
+    now = datetime.datetime.now()
+
+    is_monday = now.weekday() == 0
+
+    return render(
+        request,
+        "isitmonday.html",
+        context={"is_monday": is_monday}
+    )
