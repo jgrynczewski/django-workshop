f890a00d698916fb1660f8f2ab79fb0e0daebb06
jgrynczewski <jgrynczewski@gmail.com>
2024-12-10 10:19:05 +0100

    form version I

--- form_app/urls.py
+++ form_app/urls.py
@@ -1,1 +1,9 @@
-urlpatterns = []
+from django.urls import path
+
+from form_app import views
+
+app_name = 'form_app'
+
+urlpatterns = [
+    path("1/", views.form1, name="form1"),
+]
--- form_app/views.py
+++ form_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
+TASKS = []
+
 # Create your views here.
+def form1(request):
+    data = request.GET
+
+    task = data.get('task')
+    if task:
+        TASKS.append(task)
+
+    return render(request, "form_app/form1.html")
