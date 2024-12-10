92aee05394d7417a0458c155c9d84315e63d712b
jgrynczewski <jgrynczewski@gmail.com>
2024-12-10 16:54:43 +0100

    jupyter notebook III

--- orm_app/models.py
+++ orm_app/models.py
@@ -6,3 +6,42 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Country(models.Model):
+    name = models.CharField(max_length=64)
+    capital = models.OneToOneField('Capital', on_delete=models.CASCADE)
+
+
+class Capital(models.Model):
+    name = models.CharField(max_length=64)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Framework(models.Model):
+    name = models.CharField(max_length=64)
+    language = models.ForeignKey('Language', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.language})"
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=128)
+    actors = models.ManyToManyField('Actor')
+
+    def __str__(self):
+        return f"{self.title}"
