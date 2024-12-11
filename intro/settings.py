42c8969fd80aa123b89681a6ab2a5be011d5eb22
jgrynczewski <jgrynczewski@gmail.com>
2024-12-11 16:24:37 +0100

    async

--- intro/settings.py
+++ intro/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'extras_app.middleware.new_middleware',
 ]
 
 ROOT_URLCONF = "intro.urls"
